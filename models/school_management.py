8ff854b82b407bf092ccb96e16a4c11220df658f
krushal2206 <krushal.kalkani@brainvire.com>
2023-07-17 17:04:53 +0530

    Updated ENR Sequence

--- models/school_management.py
+++ models/school_management.py
@@ -1,6 +1,7 @@
-from odoo import fields, models, api
-from odoo.exceptions import UserError
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError, ValidationError
 from datetime import date
+
 
 class SchoolManagement(models.Model):
     _name = "school.management"
@@ -17,58 +18,33 @@ class SchoolManagement(models.Model):
         ('d', 'D'),
     ], string='Division')
     roll_number = fields.Integer(string="Roll Number")
-    enr_number = fields.Char(string="Enrollment Number", compute='_compute_enr_number', store=True,)
+
+    # ENR Number Generate
+    enr_number = fields.Char(string="Enrollment Number", copy=False, readonly=True,
+                             index=True, default=lambda self: _('New'))
+
+    @api.model
+    def create(self, vals):
+        record = super(SchoolManagement, self).create(vals)
+        record.enr_number = "ENR" + str(record.id).zfill(3)
+        return record
+
+    # Address field
     address_line1 = fields.Char(string="Address Line 1")
     address_line2 = fields.Char(string="Address Line 2")
     city = fields.Char(string="City")
-    state = fields.Many2one('res.country.state', domain="[('country_id', '=', country)]")
+    state = fields.Many2one('res.country.state',
+                            domain="[('country_id', '=', country)]")
     zip = fields.Char(string="Zip/Postal Code")
     country = fields.Many2one('res.country')
+
     phone_no = fields.Char(string="Phone Number", tracking=True, required=True)
     dob = fields.Date(string="Date of Birth", required=True)
+
+    # age
     age = fields.Integer(string='Age', compute='_compute_age', store=True)
-    parent_name = fields.Char(string="Parent Name")
-    relation = fields.Selection([
-        ('mother', 'Mother'),
-        ('father', 'Father'),
-        ('sister', 'Sister'),
-        ('brother', 'Brother'),
-        ('husband', 'Husband'),
-        ('wife', 'Wife'),
-        ('other', 'Other')
-    ], string="Relation")
-    parent_phone_number = fields.Char(string="Phone Number")
-    parent_email = fields.Char(string="Email")
-    previous_school_name = fields.Char(string="Previous School Name")
-    previous_school_enr_no = fields.Char(string="Enrollment Number")
-    admission_date = fields.Date(string="Admission Date")
-    leaving_date = fields.Date(string="Leaving Date")
-    class_teacher = fields.Many2one('teacher.management', string="Class Teacher", readonly=True)
-    stream = fields.Selection([
-        ('Science', 'Science'),
-        ('Commerce', 'Commerce'),
-        ('Arts', 'Arts')
-    ], string="Stream")
-    birth_month = fields.Char(compute='_compute_birth_month', store=True)
-    result = fields.Char()
-
-    # Allocate a class teacher
-    @api.onchange('standard', 'student_division')
-    def _onchange_standard_student_division(self):
-        if self.standard and self.student_division:
-            teacher = self.env['teacher.management'].search([
-                ('standard', '=', self.standard),
-                ('division', '=', self.student_division)
-            ], limit=1)
-            self.class_teacher = teacher.id if teacher else False
-
-    # enr number auto generate based on phone number
-    @api.depends('phone_no')
-    def _compute_enr_number(self):
-        for record in self:
-            record.enr_number = 'ENR' + record.phone_no if record.phone_no and len(record.phone_no) == 10 else ''
-
     # calculate the age
+
     @api.depends('dob')
     def _compute_age(self):
         today = fields.Date.today()
@@ -87,16 +63,57 @@ class SchoolManagement(models.Model):
         if self.dob:
             self._compute_age()
 
+    parent_name = fields.Char(string="Parent Name")
+    relation = fields.Selection([
+        ('mother', 'Mother'),
+        ('father', 'Father'),
+        ('sister', 'Sister'),
+        ('brother', 'Brother'),
+        ('husband', 'Husband'),
+        ('wife', 'Wife'),
+        ('other', 'Other')
+    ], string="Relation")
+    parent_phone_number = fields.Char(string="Phone Number")
+    parent_email = fields.Char(string="Email")
+    previous_school_name = fields.Char(string="Previous School Name")
+    previous_school_enr_no = fields.Char(string="Enrollment Number")
+    admission_date = fields.Date(string="Admission Date")
+    leaving_date = fields.Date(string="Leaving Date")
+    class_teacher = fields.Many2one(
+        'teacher.management', string="Class Teacher", readonly=True)
+    stream = fields.Selection([
+        ('Science', 'Science'),
+        ('Commerce', 'Commerce'),
+        ('Arts', 'Arts')
+    ], string="Stream")
+    birth_month = fields.Char(compute='_compute_birth_month', store=True)
+    result = fields.Char()
+    family_history = fields.Char(readonly='1')
+
+    # Allocate a class teacher
+    @api.onchange('standard', 'student_division')
+    def _onchange_standard_student_division(self):
+        if self.standard and self.student_division:
+            teacher = self.env['teacher.management'].search([
+                ('standard', '=', self.standard),
+                ('division', '=', self.student_division)
+            ], limit=1)
+            self.class_teacher = teacher.id if teacher else False
+
     # Phone number restriction
+
     @api.constrains('phone_no')
     def _check_phone_number(self):
         for record in self:
             if record.phone_no and len(record.phone_no) != 10:
-                raise UserError("Check your number; it should not exceed 10 digits.")
+                raise UserError(
+                    "Check your number; it should not exceed 10 digits.")
             if record.phone_no:
-                existing_record = self.search([('phone_no', '=', record.phone_no), ('id', '!=', record.id)], limit=1)
+                existing_record = self.search(
+                    [('phone_no', '=', record.phone_no), ('id', '!=', record.id)], limit=1)
                 if existing_record:
-                    raise UserError("This phone number already exists. Please correct it or contact the admin.")
+                    raise UserError(
+                        "This phone number already exists. Please correct it or contact the admin.")
 
     # Group by similar people of the same birthdate
     @api.depends('dob')
@@ -109,7 +126,7 @@ class SchoolManagement(models.Model):
                 record.birth_month = False
 
     def school_management_heading(self):
-        # Method logic goes here
+
         pass
 
 # server action
@@ -123,15 +140,4 @@ class SchoolManagement(models.Model):
         return {
             'type': 'ir.actions.act_url',
             'url': 'https://www.odoo.com'
-        }
-
-    def show_rainbow(self):
-        for rec in self:
-            rec.state = 'done'
-        return {
-            'effect': {
-                'fadeout': 'slow',
-                'message': 'done',
-                'type': 'rainbow_man',
-            }
         }
